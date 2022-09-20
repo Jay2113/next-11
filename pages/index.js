04e4c1eacecb438a009b9ff9d7119f2172bcb73b
Raval <jayrava@88665a48afb0.ant.amazon.com>
2022-09-20 11:11:12 -0700

    modified app

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> from AWS Amplify!
+          Welcome to <a href="https://nextjs.org">Next.js</a> from AWS Amplify for the Sev 2!
         </h1>
 
         <p className={styles.description}>
